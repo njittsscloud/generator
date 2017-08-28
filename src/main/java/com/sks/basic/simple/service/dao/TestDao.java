4418fce795b97c2941b08233dd8d2fa5b30a8e07
Ivan Shen <iusoft@sina.com>
2017-08-28 13:57:49 +0800

    添加注释和简化生成

--- src/main/java/com/sks/basic/simple/service/dao/TestDao.java
+++ src/main/java/com/sks/basic/simple/service/dao/TestDao.java
@@ -1,28 +1,15 @@
 package com.sks.basic.simple.service.dao;
 
 import com.sks.basic.simple.service.model.Test;
-import com.sks.basic.simple.service.model.TestExample;
-import java.util.List;
-import org.apache.ibatis.annotations.Param;
 
 public interface TestDao {
-    int countByExample(TestExample example);
-
-    int deleteByExample(TestExample example);
-
     int deleteByPrimaryKey(Integer id);
 
     int insert(Test record);
 
     int insertSelective(Test record);
 
-    List<Test> selectByExample(TestExample example);
-
     Test selectByPrimaryKey(Integer id);
-
-    int updateByExampleSelective(@Param("record") Test record, @Param("example") TestExample example);
-
-    int updateByExample(@Param("record") Test record, @Param("example") TestExample example);
 
     int updateByPrimaryKeySelective(Test record);
 
