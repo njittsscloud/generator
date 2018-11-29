99965cde48a52763dfe9eed115dd2b800fd5c8bc
MQG <mo1056610188@163.com>
2018-11-29 23:06:58 +0800

    report

--- src/main/java/MybatisGenerator.java
+++ src/main/java/MybatisGenerator.java
@@ -22,4 +22,12 @@ public class MybatisGenerator {
         ShellRunner.main(arg);
     }
 
+    @Test
+    public void generateReport() {
+        String config = MySqlPaginationPlugin.class.getClassLoader()
+                .getResource("mybatis-generator-cfg-report-report.xml").getFile();
+        String[] arg = {"-configfile", config, "-overwrite"};
+        ShellRunner.main(arg);
+    }
+
 }
